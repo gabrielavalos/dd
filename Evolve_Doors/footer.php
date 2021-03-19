cfb3658e1c9da7fb748e599e9ccda03b202353bd
gabrielavalos <gabby.avalos16@gmail.com>
2021-03-19 16:34:00 -0500

    footer code updated

---
+++ Evolve_Doors/footer.php
@@ -0,0 +1,66 @@
+		<?php if( is_page( 'about' ) or $post->post_parent == 9 ) {
+			include('inc/footer-about.php');
+		} ?>
+	</main>
+	
+</div><!-- Container -->
+
+
+<div id="footer-spacer"></div>
+<footer>
+	<div class="wrapper">
+		<div class="footer-symbols">
+			<div class="icon">
+				<svg version="1.1" id="icon-footer" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+					 viewBox="0 0 34 31" style="enable-background:new 0 0 34 31;" xml:space="preserve">
+				<g>
+					<path class="st0" d="M30.85,30.65H0.26v-31h33.97l-3.41,7.48H8.17v3.86H31.7l-3.41,7.48H8.17v4.7h26.09L30.85,30.65z M1.86,29.05
+						h27.95l1.95-4.28H6.56v-7.9h20.69l1.95-4.28H6.57V5.53h23.21l1.95-4.28H1.86V29.05z"/>
+				</g>
+				</svg>
+			</div>
+
+			<div class="social-media">
+				<a href="https://www.facebook.com/evolvedoors/" target="_blank">
+					<svg version="1.1" id="facebook" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 25 25" preserveAspectRatio="xMidYMid meet">
+						<g>
+							<path d="M25,12.5C25,5.6,19.4,0,12.5,0S0,5.6,0,12.5c0,6.2,4.6,11.4,10.5,12.3v-8.7H7.4v-3.6h3.2V9.7c0-3.1,1.9-4.9,4.7-4.9 c1.4,0,2.8,0.2,2.8,0.2v3.1h-1.6c-1.6,0-2,1-2,2v2.3h3.5l-0.6,3.6h-2.9v8.7C20.4,23.9,25,18.7,25,12.5z"/>
+						</g>
+					</svg>
+				</a>
+				<a href="https://www.instagram.com/evolvedoors/" target="_blank">
+					<svg version="1.1" id="instagram" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 25 25" preserveAspectRatio="xMidYMid meet">
+						<g>
+							<path d="M12.5,2.3c3.3,0,3.7,0,5.1,0.1c1.2,0.1,1.9,0.3,2.3,0.4c0.6,0.2,1,0.5,1.4,0.9c0.4,0.4,0.7,0.9,0.9,1.4 c0.2,0.4,0.4,1.1,0.4,2.3c0.1,1.3,0.1,1.7,0.1,5.1s0,3.7-0.1,5.1c-0.1,1.2-0.3,1.9-0.4,2.3c-0.2,0.6-0.5,1-0.9,1.4 c-0.4,0.4-0.9,0.7-1.4,0.9c-0.4,0.2-1.1,0.4-2.3,0.4c-1.3,0.1-1.7,0.1-5.1,0.1s-3.7,0-5.1-0.1c-1.2-0.1-1.9-0.3-2.3-0.4 c-0.6-0.2-1-0.5-1.4-0.9c-0.4-0.4-0.7-0.9-0.9-1.4c-0.2-0.4-0.4-1.1-0.4-2.3c-0.1-1.3-0.1-1.7-0.1-5.1s0-3.7,0.1-5.1 c0.1-1.2,0.3-1.9,0.4-2.3c0.2-0.6,0.5-1,0.9-1.4C4.1,3.3,4.5,3,5.1,2.8c0.4-0.2,1.1-0.4,2.3-0.4C8.8,2.3,9.2,2.3,12.5,2.3 M12.5,0 C9.1,0,8.7,0,7.3,0.1C6,0.1,5.1,0.3,4.3,0.7C3.5,1,2.8,1.4,2.1,2.1C1.4,2.8,1,3.5,0.7,4.3C0.3,5.1,0.1,6,0.1,7.3 C0,8.7,0,9.1,0,12.5c0,3.4,0,3.8,0.1,5.2c0.1,1.3,0.3,2.2,0.6,3c0.3,0.8,0.7,1.5,1.4,2.2c0.7,0.7,1.4,1.1,2.2,1.4 c0.8,0.3,1.7,0.5,3,0.6C8.7,25,9.1,25,12.5,25s3.8,0,5.2-0.1c1.3-0.1,2.2-0.3,3-0.6c0.8-0.3,1.5-0.7,2.2-1.4 c0.7-0.7,1.1-1.4,1.4-2.2c0.3-0.8,0.5-1.7,0.6-3c0.1-1.3,0.1-1.8,0.1-5.2s0-3.8-0.1-5.2c-0.1-1.3-0.3-2.2-0.6-3 c-0.3-0.8-0.7-1.5-1.4-2.2c-0.7-0.7-1.4-1.1-2.2-1.4c-0.8-0.3-1.7-0.5-3-0.6C16.3,0,15.9,0,12.5,0L12.5,0z"/>
+							<path d="M12.5,6.1C9,6.1,6.1,9,6.1,12.5s2.9,6.4,6.4,6.4s6.4-2.9,6.4-6.4S16,6.1,12.5,6.1z M12.5,16.7c-2.3,0-4.2-1.9-4.2-4.2 c0-2.3,1.9-4.2,4.2-4.2c2.3,0,4.2,1.9,4.2,4.2C16.7,14.8,14.8,16.7,12.5,16.7z"/>
+							<circle cx="19.2" cy="5.8" r="1.5"/>
+						</g>
+					</svg>
+				</a>
+			</div>
+		</div>
+
+		<div class="footer-info">
+			<div class="footer-contact">
+				<?php the_field('address', 'option'); ?><br />
+				<?php the_field('phone', 'option'); ?><br />
+				<a href="mailto:<?php the_field('email', 'option'); ?>"><?php the_field('email', 'option'); ?></a>
+			</div>
+			<div class="copyright">
+				<p>
+					<a href="<?php bloginfo('url'); ?>/privacy-policy">Privacy Policy</a> | 
+				A division of <a href="https://diamonddoors.com/" class="diamond-doors-logo" target="_blank">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/evolve/images/logos/diamond-doors-footer.png" alt="Diamond Doors" /></a>
+				</p>
+				<p>
+					&copy; <?php echo date('Y'); ?> Evolve Door Systems. All Rights Reserved. <br />Website by <a href="https://thunderstrucksales.com" target="_blank">Thunderstruck</a> // COMMENT OUT LINK" 
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<?php wp_footer(); ?> <!-- Retrieves the javascript from functions file -->
+
+</body>
+</html>
